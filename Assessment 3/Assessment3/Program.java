35ba1f0413275c50fc10c56639daa80d9020007a
Kumar R <98457182+RKprogs@users.noreply.github.com>
2024-10-07 16:05:28 +0530

    Add files via upload

---
+++ Assessment 3/Assessment3/Program.java
@@ -0,0 +1,90 @@
+package Assessment3;
+
+import java.util.Scanner;
+
+class Node{
+    int value;
+    Node next;
+
+    Node(int data){
+        this.value = data;
+        this.next = null;
+    }
+}
+
+class LinkedList{
+    private Node head;
+
+    public void insert(int data){
+        Node newNode = new Node(data);
+        if(head==null)
+            head = newNode;
+        else{
+            Node current = head;
+            while(current.next!=null)
+                current = current.next;
+            current.next = newNode;
+        }
+    }
+
+    public void displayList(){
+        Node current = head;
+        while(current!=null){
+            System.out.print(current.value);
+            System.out.print(" -> ");
+            current = current.next;
+        }
+        System.out.println(current);
+    }
+
+    public void addFirst(int data){
+        Node temp = head;
+        head = new Node(data);
+        head.next = temp;
+    }
+
+    public void insertAt(int pos, int data){
+        if(pos == 0) {
+            addFirst(data);
+            return;
+        }
+        Node current = head;
+        for(int i=0;i<pos-1;i++){
+            if(current==null){
+                System.out.println("Index out of range");
+                return;
+            }
+            current = current.next;
+        }
+        Node temp = current.next;
+        current.next = new Node(data);
+        current.next.next = temp;
+    }
+}
+
+public class Program {
+
+    public static void main(String[] args) {
+        LinkedList l1 = new LinkedList();
+        // Insert at end
+        l1.insert(141);
+        l1.insert(302);
+        l1.insert(164);
+        l1.insert(530);
+        l1.insert(474);
+
+        System.out.println("Original List");
+        l1.displayList();
+        Scanner sc = new Scanner(System.in);
+
+        //Insert at Given Key
+        System.out.println("Enter the position to insert: ");
+        int pos = sc.nextInt();
+        System.out.println("Enter the value to be inserted: ");
+        int val = sc.nextInt();
+        System.out.println("\nAfter Insertion");
+
+        l1.insertAt(pos-1,val);
+        l1.displayList();
+    }
+}
